afbc7851702db1fa179dce3b2c373ee329955dcd
AKA Math <amithjkamath@users.noreply.github.com>
2024-12-06 11:02:18 +0100

    Updating the python version

--- python_project/print_average_time.py
+++ python_project/print_average_time.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+from matplotlib.dates import HourLocator
+
 
 def print_average_time():
-    df = pd.read_csv("data/timestampsMilan.csv")
-    print(pd.to_datetime(df.arrivalTimes, "format", "%d/%m/%y %H:%M:%S") - pd.to_datetime(df.departureTimes))
+    df = pd.read_csv("datasets/timestampsMilan.csv")
+    arrivalTimes = pd.to_datetime(df.arrivalTimes, format="%d-%b-%Y %H:%M:%S")
+    departureTimes = pd.to_datetime(df.departureTimes, format="%d-%b-%Y %H:%M:%S")
 
+    durations = arrivalTimes - departureTimes
+    print("Average time spent traveling to Milan is: ", durations.mean())
+
+    durations.plot(title="DataFrame Plot")
+    #plt.figure()
+    #plt.plot(range(0, len(durations)), durations)
+    #ax = plt.subplot()
+    #ax.yaxis.set_major_locator(HourLocator())
+    #ax.yaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
+    #plt.axhline(y=durations.mean(), color="red", linestyle="-")
+    plt.show()
 
 
 if __name__ == "__main__":
